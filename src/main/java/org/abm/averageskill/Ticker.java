80631eb1fbe866742e5d342bc6e7594fff7ff6a8
Zachary D. Shaw <zdsbs@yahoo.com>
2011-02-06 11:32:47 -0500

    killing a bunch of refactored away classes

--- src/main/java/org/abm/averageskill/Ticker.java
+++
@@ -1,5 +0,0 @@
-package org.abm.averageskill;
-
-public interface Ticker {
-	public void tick(Agents agents, WorkOrders workOrders);
-}
--- src/main/java/org/abm/averageskill/TierTicker.java
+++
@@ -1,7 +0,0 @@
-package org.abm.averageskill;
-
-import java.util.List;
-
-public interface TierTicker {
-	public List<WorkOrder> tickTier(AgentsInATier agents, List<WorkOrder> workOrders);
-}
